b89745af78184be35ec7e85b7fdd49c648ba3f1f
CHristapherenese <64864560+CHristapherenese@users.noreply.github.com>
2020-09-15 17:45:36 -0400

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,73 @@
+input.touchA1.onEvent(ButtonEvent.LongClick, function () {
+    keyboard.type("!singlefile")
+})
+input.touchA2.onEvent(ButtonEvent.LongClick, function () {
+    keyboard.type("!doublefile")
+})
+input.touchA0.onEvent(ButtonEvent.LongClick, function () {
+    keyboard.type("!yellow")
+})
+function Enter () {
+    keyboard.type("$")
+    keyboard.key("enter", KeyboardKeyEvent.Press)
+}
+// Generates Driver Number
+function DriverNumber () {
+    if (input.buttonD1.wasPressed() && input.buttonD5.wasPressed()) {
+        keyboard.type("1")
+    }
+    if (input.buttonD1.wasPressed() && input.buttonD6.wasPressed()) {
+        keyboard.type("2")
+    }
+    if (input.buttonD1.wasPressed() && input.buttonD7.wasPressed()) {
+        keyboard.type("3")
+    }
+    if (input.buttonD2.wasPressed() && input.buttonD5.wasPressed()) {
+        keyboard.type("4")
+    }
+    if (input.buttonD2.wasPressed() && input.buttonD6.wasPressed()) {
+        keyboard.type("5")
+    }
+    if (input.buttonD2.wasPressed() && input.buttonD7.wasPressed()) {
+        keyboard.type("6")
+    }
+    if (input.buttonD3.wasPressed() && input.buttonD5.wasPressed()) {
+        keyboard.type("7")
+    }
+    if (input.buttonD3.wasPressed() && input.buttonD6.wasPressed()) {
+        keyboard.type("8")
+    }
+    if (input.buttonD3.wasPressed() && input.buttonD7.wasPressed()) {
+        keyboard.type("9")
+    }
+    if (input.buttonD3.wasPressed() && input.buttonD6.wasPressed()) {
+        keyboard.type("0")
+    }
+}
+// Activates Driver options to be followed by driver Number
+function DriverActions () {
+    if (input.buttonD1.wasPressed() && input.buttonD8.wasPressed()) {
+        keyboard.type("1")
+        DriverNumber()
+        Enter()
+    }
+    if (input.buttonD2.wasPressed() && input.buttonD8.wasPressed()) {
+        keyboard.type("1")
+        DriverNumber()
+        Enter()
+    }
+    if (input.buttonD3.wasPressed() && input.buttonD8.wasPressed()) {
+        keyboard.type("1")
+        DriverNumber()
+        Enter()
+    }
+    if (input.buttonD4.wasPressed() && input.buttonD8.wasPressed()) {
+        keyboard.type("1")
+        DriverNumber()
+        Enter()
+    }
+    DriverActions()
+}
+forever(function () {
+    DriverActions()
+})
